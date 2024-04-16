e59b032a73a768622bc1b6322cd4e8853ad01f59
lenne sebastien <sebastienlenne@gmail.com>
2024-04-16 13:12:50 +0200

    add folder from task 1

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,18 @@
+import $ from "jquery";
+import _ from "lodash";
+
+$(document).ready(function () {
+  $("body").append("<p>Holberton Dashboard</p>");
+  $("body").append("<p>Dashboard data for the students</p>");
+  $("body").append("<button>Click here to get started</button>");
+  $("body").append("<p id='count'></p>");
+  $("body").append("<p>Copyright - Holberton School</p>");
+
+  const updateCounter = _.debounce(function () {
+    const count = parseInt($("#count").text()) || 0;
+    $("#count").text(`${count + 1} clicks on the button`);
+  }, 500);
+
+  // Bind the debounce function to the click event on the button
+  button.on("click", updateCounter);
+});
