c18e6e73950e552ebb2fdb0d9990f47f632a7233
lenne sebastien <sebastienlenne@gmail.com>
2024-04-16 16:19:06 +0200

    update file webpack.congig.js

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -3,6 +3,9 @@ const path = require("path");
 module.exports = {
   mode: "production",
   entry: "./js/dashboard_main.js",
+  performance: {
+    maxAssetSize: 1000000,
+  },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public"),
@@ -10,18 +13,18 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|jpg|gif)$/,
+        test: /\.(gif|png|jpe?g|svg)$/i,
         use: [
+          "file-loader",
           {
-            loader: "file-loader",
+            loader: "image-webpack-loader",
             options: {
-              name: "[name].[ext]",
-              outputPath: "assets", // Placez les images dans un dossier 'assets'
-              esModule: false, // Evite d'utiliser des imports ES modules pour les images
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
             },
           },
         ],
