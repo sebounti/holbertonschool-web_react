c60393baf7d41f0b117433dc84e3d079099d5c41
lenne sebastien <sebastienlenne@gmail.com>
2024-04-16 14:45:33 +0200

    add new folder for task2

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./js/dashboard_main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "assets", // Placez les images dans un dossier 'assets'
+              esModule: false, // Evite d'utiliser des imports ES modules pour les images
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
