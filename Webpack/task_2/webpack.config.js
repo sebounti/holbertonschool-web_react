31049e450f21517847fb560c5f17870086b68d1a
lenne sebastien <sebastienlenne@gmail.com>
2024-04-21 23:27:24 +0200

    update folder task 2

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -2,9 +2,6 @@ const path = require("path");
 
 module.exports = {
   entry: "./js/dashboard_main.js",
-  performance: {
-    maxAssetSize: 1000000,
-  },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public"),
@@ -13,37 +10,17 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
+        test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(gif|png|jpe?g|svg)$/i,
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
         use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "assets/",
-            },
-          },
+          "file-loader",
           {
             loader: "image-webpack-loader",
-            options: {
-              bypassOnDebug: true, // webpack@1.x
-            },
           },
         ],
-      },
-      {
-        // Règle séparée pour le transpilage JavaScript
-        test: /\.js$/, // Appliquer le loader aux fichiers .js
-        exclude: /node_modules/, // Exclure les fichiers dans node_modules
-        use: {
-          loader: "babel-loader", // Utiliser babel-loader
-          options: {
-            presets: ["@babel/preset-env"], // Utiliser le preset-env
-          },
-        },
       },
     ],
   },
