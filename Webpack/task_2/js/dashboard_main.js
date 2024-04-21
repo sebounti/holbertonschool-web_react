970fd2a5d1fc139deab1361b957d23b7f03c1093
lenne sebastien <sebastienlenne@gmail.com>
2024-04-21 23:11:46 +0200

    update folder task 2

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -1,29 +1,21 @@
-const $ = require("jquery");
-const _ = require("lodash");
-import holbertonLogo from "../assets/holberton-logo.jpg";
+import $ from "jquery";
+import _ from "lodash";
 
 let count = 0;
-
 function updateCounter() {
   count++;
   $("#count").text(`${count} clicks on the button`);
 }
 
-const $button = $("<button>Click here to get started</button>").on(
-  "click",
-  _.debounce(updateCounter, 500, {
-    leading: true,
-    trailing: false,
-  }),
-);
+$(document).ready(() => {
+  $("body").append(`
+    <p>Holberton Dashboard</p>
+    <p>Dashboard data for the students</p>
+    <button>Click here to get started</button>
+    <p id='count'></p>
+    <p>Copyright - Holberton School</p>
+  `);
 
-// Ajout du div pour le logo en haut du corps du document
-$("body").prepend("<div id='logo'></div>");
-const $logo = $("<img>").attr("src", holbertonLogo);
-$("#logo").prepend($logo);
-
-$("body").append("<p>Holberton Dashboard</p>");
-$("body").append("<p>Dashboard data for the students</p>");
-$("body").append($button);
-$("body").append("<p id='count'></p>");
-$("body").append("<p>Copyright - Holberton School</p>");
+  const debouncedUpdateCounter = _.debounce(updateCounter, 500);
+  $("button").click(debouncedUpdateCounter);
+});
