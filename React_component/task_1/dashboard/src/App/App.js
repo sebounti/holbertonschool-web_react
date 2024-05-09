34b9767500fe971b18a0d5ff9f7b046ab08a2b2e
lenne sebastien <sebastienlenne@gmail.com>
2024-05-09 11:05:07 +0100

    update file App.js App.test.js

--- React_component/task_1/dashboard/src/App/App.js
+++ React_component/task_1/dashboard/src/App/App.js
@@ -1,73 +1,104 @@
-import React, { Component } from "react";
-import Notifications from "../Notifications/Notifications";
-import Header from "../Header/Header";
-import Login from "../Login/Login";
-import CourseList from "../CourseList/CourseList";
-import Footer from "../Footer/Footer";
+import React, { Component, Fragment } from "react";
 import PropTypes from "prop-types";
-import { getLatestNotification } from "../utils/utils";
 import "./App.css";
-
-const listCourses = [
-  { id: 1, name: "ES6", credit: 60 },
-  { id: 2, name: "Webpack", credit: 20 },
-  { id: 3, name: "React", credit: 40 },
-];
-
-const listNotifications = [
-  { id: 1, type: "default", value: "New course available" },
-  { id: 2, type: "urgent", value: "New resume available" },
-  { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
-];
+import Header from "../Header/Header.js";
+import Login from "../Login/Login.js";
+import Footer from "../Footer/Footer.js";
+import Notifications from "../Notifications/Notifications.js";
+import CourseList from "../CourseList/CourseList";
+import { getLatestNotification } from "../utils/utils";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.handleKeyCombination = this.handleKeyCombination.bind(this);
+  // Ajout de propTypes pour la propriété logOut
+  static propTypes = {
+    logOut: PropTypes.func, // La prop logOut doit être de type fonction
+  };
+
+  // Définition de defaultProps pour fournir une fonction vide par défaut à logOut
+  static defaultProps = {
+    logOut: () => {}, // Fonction vide par défaut pour la prop logOut
+  };
+
+  // Méthode appelée lorsque le composant est monté
+  componentDidMount() {
+    // Ajout d'un événement pour écouter les touches clavier pressées
+    window.addEventListener("keydown", this.handleKeyDown);
   }
 
-  handleKeyCombination(e) {
-    if (e.key === "h" && e.ctrlKey) {
+  // Méthode appelée lorsque le composant est démonté
+  componentWillUnmount() {
+    // Suppression de l'événement lors du démontage du composant
+    window.removeEventListener("keydown", this.handleKeyDown);
+  }
+
+  // Méthode pour gérer l'événement de pression des touches clavier
+  handleKeyDown = (event) => {
+    // Vérification si les touches contrôle et h sont pressées simultanément
+    if (event.ctrlKey && event.key === "h") {
+      // Affichage d'une alerte
       alert("Logging you out");
+      // Appel de la fonction logOut passée en tant que prop
       this.props.logOut();
     }
-  }
+  };
 
-  componentDidMount() {
-    document.addEventListener("keydown", this.handleKeyCombination);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener("keydown", this.handleKeyCombination);
-  }
-
+  // Méthode render() pour définir le rendu du composant
   render() {
-    const { isLoggedIn, logOut } = this.props;
+    const { isLoggedIn } = this.props;
+
+    // Déclaration des constantes listNotifications et listCourses
+    let i = 0;
+    const listNotifications = [
+      {
+        id: i++,
+        type: "default",
+        value: "New course available",
+      },
+      {
+        id: i++,
+        type: "urgent",
+        value: "New resume available",
+      },
+      {
+        id: i++,
+        type: "urgent",
+        html: { __html: getLatestNotification() },
+      },
+    ];
+
+    const listCourses = [
+      {
+        id: 1,
+        name: "ES6",
+        credit: 60,
+      },
+      {
+        id: 2,
+        name: "Webpack",
+        credit: 20,
+      },
+      {
+        id: 3,
+        name: "React",
+        credit: 40,
+      },
+    ];
+
+    // Rendu du composant avec JSX
     return (
-      <>
-        <Notifications listNotifications={listNotifications} />
+      <Fragment>
         <div className="App">
-          <Header />
-        </div>
-        <div className="App-body">
-          {!isLoggedIn ? <Login /> : <CourseList listCourses={listCourses} />}
-        </div>
-        <div className="App-footer">
+          <div className="upperside">
+            <Notifications listNotifications={listNotifications} />
+            <Header />
+          </div>
+          {isLoggedIn === false && <Login />}
+          {isLoggedIn === true && <CourseList listCourses={listCourses} />}
           <Footer />
         </div>
-      </>
+      </Fragment>
     );
   }
 }
-
-App.defaultProps = {
-  isLoggedIn: false,
-  logOut: () => {},
-};
-
-App.propTypes = {
-  isLoggedIn: PropTypes.bool,
-  logOut: PropTypes.func,
-};
 
 export default App;
--- React_component/task_1/dashboard/src/App/App.test.js
+++ React_component/task_1/dashboard/src/App/App.test.js
@@ -1,61 +1,23 @@
-import { shallow, mount } from "enzyme";
 import React from "react";
+import { expect } from "chai";
+import Adapter from "enzyme-adapter-react-16";
+import { shallow, configure } from "enzyme";
 import App from "./App";
 
-describe("<App />", () => {
-  it("App renders without crashing", () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.exists()).toEqual(true);
-  });
-  it("should contain the Notifications component", () => {
-    const wrapper = shallow(<App />);
-    wrapper.update();
-    expect(wrapper.find("Notifications")).toHaveLength(1);
-  });
-  it("should contain the Header component", () => {
-    const wrapper = shallow(<App />);
-    wrapper.update();
-    expect(wrapper.find("Header")).toHaveLength(1);
-  });
-  it("should contain the Login component", () => {
-    const wrapper = shallow(<App />);
-    wrapper.update();
-    expect(wrapper.find("Login")).toHaveLength(1);
-  });
-  it("should contain the Footer component", () => {
-    const wrapper = shallow(<App />);
-    wrapper.update();
-    expect(wrapper.find("Footer")).toHaveLength(1);
-  });
-  it("CourseList is not displayed with isLoggedIn false by default", () => {
-    const wrapper = shallow(<App />);
-    wrapper.update();
-    expect(wrapper.find("CourseList")).toHaveLength(0);
-  });
-  it("isLoggedIn is true", () => {
-    const wrapper = shallow(<App isLoggedIn />);
-    wrapper.update();
-    expect(wrapper.find("Login")).toHaveLength(0);
-    expect(wrapper.find("CourseList")).toHaveLength(1);
-  });
+configure({ adapter: new Adapter() });
 
-  it("when the keys control and h are pressed the logOut function, passed as a prop, is called and the alert function is called with the string Logging you out", () => {
-    const events = {};
-    const logout = jest.fn();
-
-    document.addEventListener = jest.fn((event, cb) => {
-      events[event] = cb;
-    });
-
-    window.alert = jest.fn();
-
-    shallow(<App logOut={logout} />);
-
-    events.keydown({ key: "h", ctrlKey: true });
-
+describe("App component", () => {
+  // Test pour vérifier la gestion de la déconnexion lors de la pression des touches contrôle et h
+  it("calls logOut function and displays alert when control and h keys are pressed", () => {
+    // Mock de la fonction logOut
+    const logOutMock = jest.fn();
+    // Création du composant avec la fonction logOut mockée
+    const wrapper = shallow(<App logOut={logOutMock} />);
+    // Simulation de la pression des touches contrôle et h
+    wrapper.find("window").simulate("keydown", { ctrlKey: true, key: "h" });
+    // Vérification que la fonction logOut a été appelée
+    expect(logOutMock).toHaveBeenCalled();
+    // Vérification que la fonction d'alerte a été appelée avec la bonne chaîne
     expect(window.alert).toHaveBeenCalledWith("Logging you out");
-    expect(logout).toHaveBeenCalled();
-
-    jest.restoreAllMocks();
   });
 });
